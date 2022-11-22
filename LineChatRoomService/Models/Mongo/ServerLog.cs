d334e608cbe35943485951eabb57e15529a3e950
MakotoAtsu <chris03152008@gmail.com>
2022-11-22 21:40:20 +0800

    Added Server Log

---
+++ LineChatRoomService/Models/Mongo/ServerLog.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LineChatRoomService.Models.Mongo
+{
+    [BsonIgnoreExtraElements]
+    public class ServerLog
+    {
+
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// unit : ms
+        /// </summary>
+        public uint ProcessTime { get; set; }
+
+        public Request Request { get; set; }
+
+        public int ResponseStatus { get; set; }
+
+        public string? Error { get; set; }
+
+    }
+
+    [BsonIgnoreExtraElements]
+    public class Request
+    {
+        public string Method { get; set; }
+
+        public string Url { get; set; }
+
+        public string? Body { get; set; }
+
+    }
+}
---
+++ LineChatRoomService/Repositories/ServerLogRepository.cs
@@ -0,0 +1,21 @@
+﻿using LineChatRoomService.Models.Mongo;
+using LineChatRoomService.Repositories.Interface;
+using MongoDB.Driver;
+
+namespace LineChatRoomService.Repositories
+{
+    public class ServerLogRepository : IServerLogRepository
+    {
+        public IMongoCollection<ServerLog> ServerLogCollection { get; }
+
+
+        public ServerLogRepository(IMongoDatabase mongoDB)
+        {
+            this.ServerLogCollection = mongoDB.GetCollection<ServerLog>("ServerLog");
+        }
+
+
+        public Task InsertLog(ServerLog log)
+            => this.ServerLogCollection.InsertOneAsync(log);
+    }
+}
